d0c14a9c048760a67cc3f0d5ac57aaba41c25707
sajad-net <sajadsajad.sch@gmail.com>
2021-02-07 02:49:21 -0500

    Banner file is modified

--- app/src/Banner.py
+++ app/src/Banner.py
@@ -1,15 +1,18 @@
-def headers():
+def headers(fun):
     import os
-    os.system("clear") if os.name == "posix" else os.system('cls')
-    from colorama import Fore,init
-    init()
-    print(Fore.RED+"""
-  ██████╗██╗  ██╗ ██████╗ ███████╗████████╗   ██╗  ██╗ 
-██╔════╝ ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝   ██║ ██╔
-██║  ███╗███████║██║   ██║███████╗   ██║█████╗█████╔╝
-██║   ██║██╔══██║██║   ██║╚════██║   ██║╚════╝██╔═██╗
-╚██████╔╝██║  ██║╚██████╔╝███████║   ██║      ██║  ██╗
- ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝      ╚═╝  ╚═╝
- """)
-    print(Fore.LIGHTBLUE_EX+"\t"*4,"GHOST-KILLER V1.0\n"\
-      ,"\t"*4,"Programming By :: "+Fore.RED+"SAJAD-CHEHRAZI\n")
+    def wrapper():
+        os.system("clear") if os.name == "posix" else os.system('cls')
+        from colorama import Fore,init
+        init()
+        print(Fore.RED+"""
+      ██████╗██╗  ██╗ ██████╗ ███████╗████████╗   ██╗  ██╗ 
+    ██╔════╝ ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝   ██║ ██╔
+    ██║  ███╗███████║██║   ██║███████╗   ██║█████╗█████╔╝
+    ██║   ██║██╔══██║██║   ██║╚════██║   ██║╚════╝██╔═██╗
+    ╚██████╔╝██║  ██║╚██████╔╝███████║   ██║      ██║  ██╗
+     ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝      ╚═╝  ╚═╝
+     """)
+        print(Fore.LIGHTBLUE_EX+"\t"*4,"GHOST-KILLER V1.0\n"\
+        ,"\t"*4,"Programming By :: "+Fore.RED+"SAJAD-CHEHRAZI\n")        fun()
+    return wrapper
+
