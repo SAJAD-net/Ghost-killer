251ed7f0615dc63d2e1ecf887994bbf5d1bffd3e
SAJAD-net <sajadsajad.sch@gmail.com>
2022-01-26 23:43:43 +0330

    whois's problems was fixed

--- app/src/whois.py
+++ app/src/whois.py
@@ -1,9 +1,10 @@
+import whois
 from colorama import Fore,init
 init()
 import requests, readline
 from src.Banner import headers
-while True:
 
+while True:
     @headers
     def whois():
         print(Fore.LIGHTBLUE_EX+'[*]-',Fore.LIGHTBLUE_EX+"`"+Fore.RED+"*"+Fore.LIGHTBLUE_EX+"` Welcome To Whois Part `"+Fore.RED+"*"+Fore.LIGHTBLUE_EX+"`")
@@ -13,9 +14,9 @@ while True:
         if Addr == "0":
             import src.chdir
         try:
-            data=requests.get('http://api.hackertarget.com/whois/?q='+Addr).text
-            print(Fore.YELLOW+"["+Fore.LIGHTRED_EX+"+"+Fore.YELLOW+"]- "+Fore.LIGHTBLUE_EX+data)
+            data = whois.whois(Addr)
+            print(Fore.YELLOW+"["+Fore.LIGHTRED_EX+"+"+Fore.YELLOW+"]- "+Fore.LIGHTBLUE_EX+data.text)
+
             input(f'{Fore.YELLOW}[{Fore.LIGHTRED_EX}+{Fore.YELLOW}]- {Fore.LIGHTBLUE_EX}┌─[Press Enter to back ... \n{Fore.LIGHTBLUE_EX}     └──╼ {Fore.YELLOW}GHOST-K {Fore.RED}✗ '+Fore.LIGHTBLUE_EX)
         except:
-            input(f'{Fore.LIGHTRED_EX}[{Fore.LIGHTBLUE_EX}+{Fore.LIGHTRED_EX}]- {Fore.LIGHTBLUE_EX}┌─[Error, Press Enter to back ... \n{Fore.LIGHTBLUE_EX}     └──╼ {Fore.
-            }GHOST-K {Fore.RED}✗ '+Fore.LIGHTBLUE_EX)
+            input(f'{Fore.LIGHTRED_EX}[{Fore.LIGHTBLUE_EX}+{Fore.LIGHTRED_EX}]- {Fore.LIGHTBLUE_EX}┌─[Error, Press Enter to back ... \n{Fore.LIGHTBLUE_EX}     └──╼ {Fore.YELLOW}GHOST-K {Fore.RED}✗ '+Fore.LIGHTBLUE_EX)
