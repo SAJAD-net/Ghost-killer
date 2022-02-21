b4a2623c1da17cbbf58322cdf315abaff7ee565d
SAJAD-net <sajadsajad.sch@gmail.com>
2022-02-21 16:43:22 +0330

    Some additional items in GHOSTKILLER.py were removed

--- app/GHOSTKILLER.py
+++ app/GHOSTKILLER.py
@@ -1,73 +1,50 @@
 import os, sys, readline
+from src.Banner import headers
 from colorama import Fore,init
 init()
-from src.Banner import headers
+
 
 @headers
-def GHOSTKILLER(): 
+def GHOSTKILLER():
     while True:
-        print(Fore.YELLOW+'[*]-',Fore.LIGHTBLUE_EX+"`"+Fore.RED+"*"+Fore.LIGHTBLUE_EX+"` Welcome To GHOST-KILLER (H4CK3R) V1.0 `"+Fore.RED+"*"+Fore.LIGHTBLUE_EX+"`\n")
-        Tools=["cloudeflare",
-        "httpHeaders",
-        "iplocation",
-        "reversip",
-        "dnsLookup",
-        "portScanner",
-        "cmsDetect",
-        "robotsScanner",
-        "traceRoute",
-        "sharedDnss",
-        "whois",
-        "adminPages"]
-        while True:
-            count = 1
-            c=0
-            for i in range(len(Tools)):
-                if len(str(count)) == 1:
-                    if count % 3 == 0 or count == 5 or count == 7:
-                        if count != 6:
-                            print("")
-                    print(Fore.LIGHTRED_EX+f'\t[0{count}]-',Fore.LIGHTBLUE_EX+Tools[c],"\t",end="")
-                    count+=1
-                    c+=1
-                elif count == 11:
-                    print(Fore.LIGHTRED_EX+f'\n\t[{count}]-',Fore.LIGHTBLUE_EX+Tools[c],"\t",end="")
-                    count+=1
-                    c+=1
-                elif count == 12:
-                    print(Fore.LIGHTRED_EX+f'\t\t[{count}]-',Fore.LIGHTBLUE_EX+Tools[c],"\t",end="\n") 
-                    break
-                elif c == 5:
-                    print("\t",end="")
-                else:
-                    print(Fore.LIGHTRED_EX+f'\t[{count}]-',Fore.LIGHTBLUE_EX+Tools[c],"\t",end="") 
-                    count+=1
-                    c+=1
-            Self=input(Fore.YELLOW+'\n[!]-'+Fore.LIGHTBLUE_EX+' ┌─[enter number to chose tool ((:\n'+Fore.LIGHTBLUE_EX+'     └──╼ '+Fore.YELLOW+'GHOST-K '+Fore.RED+'✗ '+Fore.LIGHTBLUE_EX)
-            if Self == '1' or Self == '01':
-                import src.cloudflare.cloudeflare
-            elif Self == '2' or Self == '02':
-                import src.httpheaders.httpheaders
-            elif Self == '3' or Self == '03':
-                import src.iplocation.iplocation
-            elif Self == '4' or Self == '04':
-                import src.reversip.reversip
-            elif Self == '5' or Self == '05':
-                import src.dnslookup.dnslookup
-                
-            elif Self == '6' or Self == "06":
-                import src.portscanner.portscanner
-            elif Self == '7' or Self == "07":
-                import src.cms.cms
-            elif Self == '8' or Self == "08":
-                import src.robots.robots
-            elif Self == '9' or Self == "09":
-                import src.traceroute.traceroute
-            elif Self == '10' or Self == "10":
-                import src.findsharedns.fsd
-            elif Self == '11' or Self == "11":
-                import src.whois.whois
-            elif Self == '12' or Self == "12":
-                import src.finder.finder
-            else:
-                exit()
+        print(Fore.YELLOW+'[*]-',Fore.LIGHTBLUE_EX+"`"+Fore.RED+"*"+\
+        Fore.LIGHTBLUE_EX+"` Welcome To GHOST-KILLER (H4CK3R) V1.0 `"+Fore.RED+"*"+Fore.LIGHTBLUE_EX+"`")
+
+        rednum = Fore.LIGHTRED_EX
+        bluene = Fore.LIGHTBLUE_EX
+        print(f"""
+        {rednum}[01]- {bluene}cloudeFlare 		{rednum}[02]- {bluene}httpHeaders
+        {rednum}[03]- {bluene}IPlocation 		{rednum}[04]- {bluene}reversIP
+        {rednum}[05]- {bluene}dnsLookup 		{rednum}[06]- {bluene}portScanner
+        {rednum}[07]- {bluene}cmsDetect 		{rednum}[08]- {bluene}robotsScanner
+        {rednum}[09]- {bluene}traceRoute 		{rednum}[10]- {bluene}sharedDns
+        {rednum}[11]- {bluene}whois             \t{rednum}[12]- {bluene}adminPages""")
+
+        Self=input(Fore.YELLOW+'\n[!]-'+Fore.LIGHTBLUE_EX+' ┌─[choose the tool number ((:\n'+Fore.LIGHTBLUE_EX+'     └──> '+Fore.YELLOW+'GHOST-K '+Fore.RED+'✗ '+Fore.LIGHTBLUE_EX)
+
+        if Self == '1':
+            import src.cloudflare.cloudeflare
+        elif Self == '2':
+            import src.httpheaders.httpheaders
+        elif Self == '3':
+            import src.iplocation.iplocation
+        elif Self == '4':
+            import src.reversip.reversip
+        elif Self == '5':
+            import src.dnslookup.dnslookup
+        elif Self == '6':
+            import src.portscanner.portscanner
+        elif Self == '7':
+            import src.cms.cms
+        elif Self == '8' :
+            import src.robots.robots
+        elif Self == '9' :
+            import src.traceroute.traceroute
+        elif Self == '10':
+            import src.findsharedns.fsd
+        elif Self == '11':
+            import src.whois.whois
+        elif Self == '12':
+            import src.finder.finder
+        else:
+            sys.exit()
